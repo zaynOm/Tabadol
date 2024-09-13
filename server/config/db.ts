1fbede906b1742efb0015cda724029ca567748ac
zaynom <ouaziz0626@gmail.com>
2024-09-13 23:19:16 +0100

    database connection config

---
+++ server/config/db.ts
@@ -0,0 +1,19 @@
+import { config } from "dotenv";
+import mongoose from "mongoose";
+
+config();
+
+const connectDB = async (): Promise<void> => {
+  try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error('The "MONGODB_URI" environment variable is not defined');
+    }
+
+    await mongoose.connect(process.env.MONGODB_URI, { dbName: "tabadol_db" });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default connectDB;
