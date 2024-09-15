c11f6cf4f55268cd4c6481d42984ab7fbe09c684
zaynom <ouaziz0626@gmail.com>
2024-09-15 21:01:57 +0100

    Rename files

--- server/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { googleCallback, googleLogin } from "../controllers/authController";
-
-const router = express.Router();
-
-router.get("/auth/google", googleLogin);
-router.get("/auth/google/callback", googleCallback);
-
-export default router;
