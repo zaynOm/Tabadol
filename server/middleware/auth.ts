27dd2c5ff32bc7de91054b0e608956771fdb7aec
zaynom <ouaziz0626@gmail.com>
2024-09-16 00:04:38 +0100

    Add auth checking middleware

---
+++ server/middleware/auth.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.split(" ")[1];
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET as string, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "unauthorized" });
+    }
+    console.log(decoded);
+    req.user = decoded;
+    next();
+  });
+};
