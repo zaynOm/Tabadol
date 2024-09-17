edec04f699882e403daca23ecf9a05d31252f841
zaynom <ouaziz0626@gmail.com>
2024-09-17 11:22:08 +0100

    Add exchange demand model

--- server/models/exchangeDemand.ts
+++ server/models/exchangeDemand.ts
@@ -3,7 +3,7 @@ import { model, Schema, Types } from "mongoose";
 type TLocation = {
   academy: Types.ObjectId;
   province: Types.ObjectId;
-  commune: Types.ObjectId;
+  commune: Types.ObjectId | Types.ObjectId[];
   school: string;
 };
 
@@ -16,22 +16,32 @@ type TDemand = {
   status: "open" | "closed" | "matched";
 };
 
+const locationSchema = new Schema<TLocation>(
+  {
+    academy: { type: Schema.Types.ObjectId, ref: "Academy", required: true },
+    province: { type: Schema.Types.ObjectId, ref: "Province", required: true },
+    commune: { type: Schema.Types.ObjectId, ref: "Commune", required: true },
+    school: { type: String, required: true },
+  },
+  { _id: false },
+);
+
 const exchangeDemandSchema = new Schema<TDemand>(
   {
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     post: { type: String, required: true },
     speciality: { type: String, required: true, default: "sans" },
-    currentLocation: {
-      academy: { type: Schema.Types.ObjectId, ref: "Academy", required: true },
-      province: { type: Schema.Types.ObjectId, ref: "Province", required: true },
-      commune: { type: Schema.Types.ObjectId, ref: "Commune", required: true },
-      school: { type: String, required: true },
-    },
+    currentLocation: { type: locationSchema, required: true },
     desiredLocation: {
-      academy: { type: Schema.Types.ObjectId, ref: "Academy", required: true },
-      province: { type: Schema.Types.ObjectId, ref: "Province", required: true },
-      commune: { type: Schema.Types.ObjectId, ref: "Commune", required: true },
-      school: { type: String, required: true },
+      type: new Schema<TLocation>(
+        {
+          ...locationSchema.obj,
+          commune: [{ type: Schema.Types.ObjectId, ref: "Commune" }],
+          school: String,
+        },
+        { _id: false },
+      ),
+      required: true,
     },
     status: { type: String, enum: ["open", "closed", "matched"], default: "open" },
   },
