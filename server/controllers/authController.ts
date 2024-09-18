211142f0400401ad7422683158037c15cfb397a1
zaynom <ouaziz0626@gmail.com>
2024-09-18 11:07:03 +0100

    Use async error handler to get rid of try/catch

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -2,37 +2,30 @@ import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { oauth2Client } from "../config/auth";
 import User, { TUser } from "../models/user";
+import asyncErrorHandler from "../utils/asyncErrorHandler";
 
-
-export const googleAuth = async (req: Request, res: Response) => {
+export const googleAuth = asyncErrorHandler(async (req: Request, res: Response) => {
   const { idToken } = req.body;
 
-  try {
-    const ticket = await oauth2Client.verifyIdToken({
-      idToken,
-      audience: process.env.GOOGLE_CLIENT_ID,
-    });
-    const payload = ticket.getPayload();
+  const ticket = await oauth2Client.verifyIdToken({
+    idToken,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
+  const payload = ticket.getPayload();
 
-    const userData: TUser = {
-      googleId: payload?.sub as string,
-      email: payload?.email as string,
-      name: payload?.name as string,
-      picture: payload?.picture as string,
-    };
+  const userData: TUser = {
+    googleId: payload?.sub as string,
+    email: payload?.email as string,
+    name: payload?.name as string,
+    picture: payload?.picture as string,
+  };
 
-    let user = await User.findOne({ email: userData.email });
-    if (!user) {
-      user = await User.create(userData);
-    }
-
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!);
-
-    return res.json({ token, user });
-  } catch (err) {
-    console.error(err);
-    return res
-      .status(400)
-      .json({ message: "Error retrievin user data", error: (err as Error).message });
+  let user = await User.findOne({ email: userData.email });
+  if (!user) {
+    user = await User.create(userData);
   }
-};
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!);
+
+  res.json({ success: true, data: { token, user } });
+});
