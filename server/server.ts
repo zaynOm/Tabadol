ad6ca76fefd136731b1efdd1c62df24342ddc65f
zaynom <ouaziz0626@gmail.com>
2024-09-18 10:10:12 +0100

    Add error handling middleware

--- server/server.ts
+++ server/server.ts
@@ -25,6 +25,16 @@ app.all("*", (req, _res, next) => {
   next(err);
 });
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: CustomError, _req: Request, res: Response, _next: NextFunction) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message.replace(/"/g, "'"),
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server listening on port 5000");
