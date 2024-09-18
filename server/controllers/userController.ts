4a3e4de7f8e7ed4f0930aa930cab9d13a657408a
zaynom <ouaziz0626@gmail.com>
2024-09-18 11:09:21 +0100

    Add update exchange demand controller

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,6 +1,7 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import ExchangeDemand from "../models/exchangeDemand";
 import asyncErrorHandler from "../utils/asyncErrorHandler";
+import CustomError from "../utils/customError";
 
 // export const getUser = async (req: Request, res: Response)=>{
 //
@@ -19,3 +20,20 @@ export const getExchangeDemands = asyncErrorHandler(async (req: Request, res: Re
   const demands = await ExchangeDemand.find(query);
   res.json({ success: true, data: demands });
 });
+
+export const updateExchangeDemand = asyncErrorHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+
+    const updatedDemand = await ExchangeDemand.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedDemand) {
+      const error = new CustomError("ExchangeDemand not found!", 404);
+      return next(error);
+    }
+
+    res.json({ success: true, data: updatedDemand });
+  },
+);
