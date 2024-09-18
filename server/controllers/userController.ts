6a0dfb611534519911d75e1562f9b360ecad1291
zaynom <ouaziz0626@gmail.com>
2024-09-18 11:08:34 +0100

    Clean up the response

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -10,12 +10,12 @@ export const createExchangeDemand = asyncErrorHandler(async (req: Request, res: 
   const data = req.body;
 
   const demand = await ExchangeDemand.create(data);
-  return res.json({ success: true, demand });
+  res.json({ success: true, data: demand });
 });
 
 export const getExchangeDemands = asyncErrorHandler(async (req: Request, res: Response) => {
   const query = req.query;
 
   const demands = await ExchangeDemand.find(query);
-  return res.json({ success: true, demands });
+  res.json({ success: true, data: demands });
 });
