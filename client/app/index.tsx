2f469a6166351cd81c37a0e16a66d187d0621ab0
zaynom <ouaziz0626@gmail.com>
2024-09-19 18:55:19 +0100

    Better setup with nativewind

---
+++ client/app/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View className="flex-1 items-center p-24">
+      <View>
+        <Text className="text-4xl font-bold">Hello World</Text>
+        <Text>This is the first page of your app.</Text>
+      </View>
+    </View>
+  );
+}
