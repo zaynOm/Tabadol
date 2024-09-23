3d55c98ab0c1f97c493fd105b053a81f607f3b90
zaynom <ouaziz0626@gmail.com>
2024-09-23 23:51:00 +0100

    Add local auth routes

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { googleAuth } from "../controllers/authController";
+import { googleAuth, signIn, signUp } from "../controllers/authController";
 
 const router = express.Router();
 
 // router.get("/auth/google", googleLogin);
 router.post("/auth/google", googleAuth);
+router.post("/auth/signup", signUp);
+router.post("/auth/login", signIn);
 
 export default router;
