c7f3a15e65d0496c4c78b815602b37ba80af013c
zaynom <ouaziz0626@gmail.com>
2024-09-23 23:50:44 +0100

    Update User model to work with local jwt auth

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,20 +1,22 @@
 import mongoose from "mongoose";
 
 export type TUser = {
-  googleId: string;
+  googleId?: string;
   name: string;
   email: string;
-  picture: string;
+  picture?: string;
+  password?: string;
 };
 
 const userSchema = new mongoose.Schema<TUser>(
   {
-    googleId: { type: String, required: true, unique: true },
+    googleId: { type: String, sparse: true, unique: true },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     picture: String,
+    password: String,
   },
-  { timestamps: true },
+  { timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
