6d18f22f90be001d5bba345b18f46160eee16a43
zaynom <ouaziz0626@gmail.com>
2024-09-24 19:00:52 +0100

    Move types to shared

--- server/models/exchangeDemand.ts
+++ server/models/exchangeDemand.ts
@@ -1,51 +1,36 @@
-import { model, Schema, Types } from "mongoose";
+import { Demand, Location } from "@/types/demand";
+import { model, Schema } from "mongoose";
 
-type TLocation = {
-  academy: Types.ObjectId;
-  province: Types.ObjectId;
-  commune: Types.ObjectId | Types.ObjectId[];
-  school: string;
-};
-
-type TDemand = {
-  userId: Types.ObjectId;
-  post: string;
-  speciality: string;
-  currentLocation: TLocation;
-  desiredLocation: TLocation;
-  status: "open" | "closed" | "matched";
-};
-
-const locationSchema = new Schema<TLocation>(
+const locationSchema = new Schema<Location>(
   {
     academy: { type: Schema.Types.ObjectId, ref: "Academy", required: true },
     province: { type: Schema.Types.ObjectId, ref: "Province", required: true },
     commune: { type: Schema.Types.ObjectId, ref: "Commune", required: true },
     school: { type: String, required: true },
   },
-  { _id: false },
+  { _id: false }
 );
 
-const exchangeDemandSchema = new Schema<TDemand>(
+const exchangeDemandSchema = new Schema<Demand>(
   {
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     post: { type: String, required: true },
     speciality: { type: String, required: true, default: "sans" },
     currentLocation: { type: locationSchema, required: true },
     desiredLocation: {
-      type: new Schema<TLocation>(
+      type: new Schema<Location>(
         {
           ...locationSchema.obj,
           commune: [{ type: Schema.Types.ObjectId, ref: "Commune" }],
           school: String,
         },
-        { _id: false },
+        { _id: false }
       ),
       required: true,
     },
     status: { type: String, enum: ["open", "closed", "matched"], default: "open" },
   },
-  { timestamps: true },
+  { timestamps: true }
 );
 
 const ExchangeDemand = model("ExchangeDemand", exchangeDemandSchema);
