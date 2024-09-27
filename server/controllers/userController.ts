abc9bb71f8b03efaedf7e2a9fd897ac227df6a54
zaynom <ouaziz0626@gmail.com>
2024-09-27 09:04:56 +0100

    Update user profile

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,39 +1,13 @@
-import { NextFunction, Request, Response } from "express";
-import ExchangeDemand from "../models/exchangeDemand";
+import { Request, Response } from "express";
+import User from "../models/user";
 import asyncErrorHandler from "../utils/asyncErrorHandler";
-import CustomError from "../utils/customError";
 
-// export const getUser = async (req: Request, res: Response)=>{
-//
-// }
+export const updateUser = asyncErrorHandler(async (req: Request, res: Response) => {
+  const { id } = req.params;
 
-export const createExchangeDemand = asyncErrorHandler(async (req: Request, res: Response) => {
-  const data = req.body;
+  console.log("🚀 ~ file: userController.ts:11 ~ body:", req.body);
 
-  const demand = await ExchangeDemand.create(data);
-  res.json({ success: true, data: demand });
+  const user = await User.findByIdAndUpdate(id, req.body, { new: true });
+
+  res.json({ success: true, data: user });
 });
-
-export const getExchangeDemands = asyncErrorHandler(async (req: Request, res: Response) => {
-  const query = req.query;
-
-  const demands = await ExchangeDemand.find(query);
-  res.json({ success: true, data: demands });
-});
-
-export const updateExchangeDemand = asyncErrorHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.params;
-
-    const updatedDemand = await ExchangeDemand.findByIdAndUpdate(id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!updatedDemand) {
-      const error = new CustomError("ExchangeDemand not found!", 404);
-      return next(error);
-    }
-
-    res.json({ success: true, data: updatedDemand });
-  },
-);
