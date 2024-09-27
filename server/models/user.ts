7cb3bdcd177e81c1ab33ef0ef4ea150630bc106a
zaynom <ouaziz0626@gmail.com>
2024-09-27 09:07:02 +0100

    Move post, speciality, currentLocation fields to the user collection

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,4 +1,6 @@
+import { Location } from "@/types/demand";
 import mongoose from "mongoose";
+import { locationSchema } from "./exchangeDemand";
 
 export type TUser = {
   googleId?: string;
@@ -6,6 +8,9 @@ export type TUser = {
   email: string;
   picture?: string;
   password?: string;
+  post: string;
+  speciality: string;
+  location?: Location | null;
 };
 
 const userSchema = new mongoose.Schema<TUser>(
@@ -13,11 +18,26 @@ const userSchema = new mongoose.Schema<TUser>(
     googleId: { type: String, sparse: true, unique: true },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    picture: String,
-    password: String,
+    password: {
+      type: String,
+      required: function () {
+        return !this.googleId;
+      },
+    },
+    picture: { type: String, default: null },
+    post: { type: String, default: null },
+    speciality: { type: String, default: "sans" },
+    location: { type: locationSchema, default: null },
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", function (next) {
+  if (this.location === undefined) {
+    this.location = null;
+  }
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
