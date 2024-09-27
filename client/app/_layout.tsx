fedfb074578120b1b09fbd6f994bf46a6ba51534
zaynom <ouaziz0626@gmail.com>
2024-09-27 08:48:11 +0100

    Add search route

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,13 +1,31 @@
 import { AuthProvider } from "@/context/AuthContext";
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { Text, TouchableOpacity } from "react-native";
 
 const RootLayout = () => {
   return (
     <AuthProvider>
-      <Stack>
+      <Stack screenOptions={{ statusBarTranslucent: true }}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="(search)"
+          options={{
+            title: "Filter",
+            headerShown: false,
+            animation: "slide_from_right",
+            contentStyle: {
+              backgroundColor: "white",
+            },
+            headerShadowVisible: false,
+            headerRight: () => (
+              <TouchableOpacity>
+                <Text>Reset</Text>
+              </TouchableOpacity>
+            ),
+          }}
+        />
       </Stack>
     </AuthProvider>
   );
