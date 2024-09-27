1a104534e7b7e7a9951d9eee62cb014a54985aa8
zaynom <ouaziz0626@gmail.com>
2024-09-27 08:55:15 +0100

    Constants

---
+++ client/constants/index.ts
@@ -0,0 +1,42 @@
+import profile from "@/assets/images/profile.png";
+import user from "@/assets/images/user.png";
+
+export const images = {
+  profile,
+  user,
+};
+
+export const posts = [
+  {
+    _id: "1",
+    name: "Professeur d'enseignement primaire",
+  },
+  {
+    _id: "2",
+    name: "Professeur d'enseignement secondaire préparatoire",
+  },
+  {
+    _id: "3",
+    name: "Professeur d'enseignement secondaire",
+  },
+  {
+    _id: "4",
+    name: "Professeur éminent d'éducation et de formation",
+  },
+  {
+    _id: "5",
+    name: "Professeur d'éducateur de l'enseignement primaire",
+  },
+  {
+    _id: "6",
+    name: "Professeur spécialisé en cadre pédagogique",
+  },
+  {
+    _id: "7",
+    name: "Professeur spécialisé en sciences sociales",
+  },
+  {
+    _id: "8",
+    name: "Professeur spécialisé en économie et gestion",
+  },
+];
